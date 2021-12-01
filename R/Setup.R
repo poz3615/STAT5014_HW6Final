039b16138e8d0f0bea4cf4ed50bd39e179b9db57
poz3615 <90327601+poz3615@users.noreply.github.com>
2021-12-01 11:27:15 -0500

    Update Setup.R

--- R/Setup.R
+++ R/Setup.R
@@ -5,44 +5,35 @@ shinyUI(pageWithSidebar(
   headerPanel("Flexdashboard"),
   
   sidebarPanel(
-    selectInput("symbol", "Select your portfolio", 
-                choices = c("AAPL","TSLA","SPY","PG","TWTR"), selected = c("TSLA","PG", "TWTR"),
+    selectInput("symbol", "Stock", 
+                choices = c("AAPL","MRNA","NKE","CMG"), selected = c("AAPL","MRNA","NKE","CMG"),
                 multiple = TRUE), 
     
     dateRangeInput('dateRange',
-                   label = 'Date range input: yyyy-mm-dd',
+                   label = 'Date of Stock: yyyy-mm-dd',
                    start = '2021-11-01', end = Sys.Date()),
     
     conditionalPanel(
       condition = "input.symbol.indexOf('AAPL') > -1",
-      numericInput("var1","weight of the AAPL","0")
+      numericInput("var1","Investment AAPL","0")
     ),
     
     conditionalPanel(
       condition = "input.symbol.indexOf('TSLA') > -1",
-      numericInput("var2","weight of the TSLA","0")
+      numericInput("var2","Investment MRNA","0")
     ),
     
     conditionalPanel(
       condition = "input.symbol.indexOf('SPY') > -1",
-      numericInput("var3","weight of the SPY","0")
+      numericInput("var3","Investment NKE","0")
     ),
     
      
     conditionalPanel(
       condition = "input.symbol.indexOf('PG') > -1",
-      numericInput("var4","weight of the PG","0")
+      numericInput("var4","Investment CMG","0")
     ),
-    
-    conditionalPanel(
-      condition = "input.symbol.indexOf('TWTR') > -1",
-      numericInput("var5","weight of the TWTR","0")
-    )
-    
-  ),
-  
-  
-  
+   
   mainPanel(
     
     tabsetPanel(
